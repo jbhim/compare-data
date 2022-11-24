8b3f7221ae9c4d3365919dfe7187305635e292f0
jint <jint@ywsoftware.com>
2022-11-24 18:26:42 +0800

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+import { setupCounter } from './counter'
+
+export default {
+    a: () => {
+        console.log('1111')
+    },
+    setupCounter
+}
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'lib/index.ts'),
+            name: 'compare-data',
+            // the proper extensions will be added
+            fileName: 'compare-data'
+        },
+        rollupOptions: {
+            // 确保外部化处理那些你不想打包进库的依赖
+            external: ['lodash'],
+            output: {
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    lodash: 'lodash'
+                }
+            }
+        }
+    }, resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            '@src': resolve(__dirname, 'src'),
+            '@lib': resolve(__dirname, 'lib'),
+        }
+    }
+})
